0bdb93bc8e6868047ed53216d846a43c0bee020a
HeitorJr <heitor.adao@gmail.com>
2023-08-05 08:30:51 -0300

    Change variable names

--- exe/compose.rb
+++ exe/compose.rb
@@ -6,10 +6,10 @@ require 'chunky_png'
 
 # Creating an image from scratch, save as an interlaced PNG
 
-avatar = ChunkyPNG::Image.from_file('input/avatar.png')
-badge  = ChunkyPNG::Image.from_file('input/ruby.png')
+base = ChunkyPNG::Image.from_file('input/avatar.png')
+feature  = ChunkyPNG::Image.from_file('input/ruby.png')
 
-avatar.compose!(badge, 10, 10)
-avatar.save('output/composited-fast.png', :fast_rgba)
-avatar.save('output/composited-default.png')
+base.compose!(feature, 10, 10)
+base.save('output/composited-fast.png', :fast_rgba)
+base.save('output/composited-default.png')
 
