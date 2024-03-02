87d690696b9d643c7109a29837fb4f1ef7e28980
Heitor Jr <heitor.adao@gmail.com>
2024-03-02 20:58:53 -0300

    Add help

--- exe/from_scratch.rb
+++ exe/from_scratch.rb
@@ -3,12 +3,13 @@
 
 require 'chunky_png'
 
-# Creating an image from scratch, save as an interlaced PNG
+puts "Creating a transparent png from scratch"
 
 png = ChunkyPNG::Image.new(16, 16, ChunkyPNG::Color::TRANSPARENT)
 png[1,1] = ChunkyPNG::Color.rgba(10, 20, 30, 128)
 png[2,1] = ChunkyPNG::Color('black @ 0.5')
 
 #png.save('filename.png')
-png.save('output/filename.png', :interlace => false)
+puts "Generating output/transparent.png"
+png.save('output/transparent.png', :interlace => false)
 
